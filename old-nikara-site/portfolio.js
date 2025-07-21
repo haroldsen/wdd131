17047c0ab6fa1aa00595076daf43035075b673df
Christian Haroldsen <ctharoldsen@gmail.com>
2025-07-20 23:48:04 -0600

    Major changes to Nikara site.

---
+++ old-nikara-site/portfolio.js
@@ -0,0 +1,252 @@
+
+const aboutMeContent = `
+    <div class="block-item">
+        <h3>ABOUT ME</h3>
+        <div class="row">
+            <button id="<"><</button>
+            <div class="column">
+                <img id="about-me-img" src="media/grad-pic.jpg">
+                <p id="about-me-text">
+                    I graduated from BYU-Idaho in July of 2025 with a degree in child development.
+                </p>
+            </div>
+            <button id=">">></button>
+        </div>
+    </div>
+
+    <div class="block-item">
+        <h3>MY RESUME</h3>
+        <a href="media/resume.pdf" target="_blank">Check it out!</a>
+    </div>
+
+    <div class="block-item">
+        <h3>THIS I KNOW</h3>
+        <p>
+            This document outlines my core beliefs as a child development professional.
+            I think it's important to reflect on our core beliefs because they guide our interactions with children.
+            From this document, you will see that I believe we have a profound influence on children.
+        </p>
+        <a href="media/this-i-know.pdf" target="_blank">Check it out!</a>
+    </div>
+
+    <div class="block-item">
+        <h3>PROFESSIONAL PARADIGM</h3>
+        <p>
+            This document highlights four child development theorists and how they have influenced my own personal philosophies.
+            I think it's important to know about these theorists because their theories have shaped the ways we view and treat children.
+            When looking at this document, you will discover that I believe in the importance of knowing developmental stages, scaffolding learning, modeling behavior, and helping children develop healthy attachments.
+        </p>
+        <a href="media/pro-paradigm.pdf" target="_blank">Check it out!</a>
+    </div>
+`;
+
+const personalEthicsContent = `
+    <div class="block-item">
+        <h3>CODE OF ETHICS</h3>
+        <img src="media/ethics.jpg">
+    </div>
+
+    <div class="block-item">
+        <h3>MY ETHICAL COMMITMENT</h3>
+        <p>
+            This document outlines my commitment to be ethical in my interactions with children, families, colleagues, and society.
+            I believe ethical commitment is crucial because children are the future.  We cannot afford to mistreat or misdirect their potential.
+            When reviewing this document, you will find that I value creating a legacy of integrity.
+        </p>
+        <a href="media/my-ethical-commitment.pdf" target="_blank">Check it out!</a>
+    </div>
+`;
+
+const intentionalTeacherContent = `
+    <div class="block-item">
+        <h3>INTENTIONAL TEACHER</h3>
+        <img src="media/thinker.jpg">
+    </div>
+
+    <div class="block-item">
+        <h3>DEVELOPMENTALLY APPROPRIATE PRACTICE</h3>
+        <p>
+            This document outlines a principle of developmentally appropriate practice I value.  These principles were instituted by the NAEYC.
+            It is important to be aware of DAP principles because these tools can be used to better understand children and how to help them learn effectively.
+            While reviewing this document, you will find that I value fostering a child's sense of purpose and belonging by connecting their learning to their home and family life.
+        </p>
+        <a href="media/dap-paper.pdf" target="_blank">Check it out!</a>
+    </div>
+
+    <div class="block-item">
+        <h3>HOME ASSESSMENT</h3>
+        <p>
+            A home assessment is an opportunity for a child development specialist to visit with a parent while observing a child's home interaction.  For this project, I had the opportunity to perform an assessment.
+            Home assessments are valuable because they are an opportunity to lend support to parents in the home.
+            From this document, you can observe that I value understanding the different aspects of a child's life that I may more effectively teach to their understanding and needs.
+        </p>
+        <a href="media/home-assessment.pdf" target="_blank">Check it out!</a>
+    </div>
+
+    <div class="block-item">
+        <h3>SOLO LESSON PLAN</h3>
+        <p>
+            This document is a lesson plan that was used in the BYU-Idaho toddler labs.
+            For me, lesson plans are important because they are an opportunity to provide structure and purpose to a child's learning.
+            While planning lessons, I value teaching to the specific needs of children, including goal setting, and planning for all domains of development.
+        </p>
+        <a href="media/solo-plan.pdf" target="_blank">Check it out!</a>
+    </div>
+`;
+
+const skilledCollaboratorContent = `
+    <div class="block-item">
+        <h3>TEAM LESSON PLAN</h3>
+        <p>
+            This is a lesson plan for the BYU-Idaho toddler labs done in collaboration with another teacher.
+            Lesson planning as a team is important because fellow teachers may have additional insight into how best to help an individual child.
+            I value the opportunities to work with others to create ideas as a team.
+        </p>
+        <a href="media/team-plan.pdf" target="_blank">Check it out!</a>
+    </div>
+
+    <div class="block-item">
+        <h3>STAFF TRAINING PRESENTATION</h3>
+        <p>
+            This is a visual presentation used in a staff training simulation.
+            Knowing how to conduct staff trainings is valuable because there is a notable difference in the performance of a unified staff.
+            I enjoy opportunities to discuss the values that will shape the success of a working team.
+        </p>
+        <a href="media/staff-training-presentation.pdf" target="_blank">Check it out!</a>
+    </div>
+`;
+
+const observationAndAssessmentContent = `
+    <div class="block-item">
+        <h3>MY ETHICAL COMMITMENT</h3>
+        <p>
+            This document outlines my commitment to be ethical in my interactions with children, families, colleagues, and society.
+            I believe ethical commitment is crucial because children are the future.  We cannot afford to mistreat or misdirect their potential.
+            When reviewing this document, you will find that I value creating a legacy of integrity.
+        </p>
+        <a href="media/my-ethical-commitment.pdf" target="_blank">Check it out!</a>
+    </div>
+
+    <div class="block-item">
+        <h3>ECERS</h3>
+        <p>
+            This is the Early Childhood Environment Rating Scale which is used to measure the quality of a childhood program.
+            ECERS is important because it helps programs know where they can make improvements.
+            This document shows my proficiency in conducting this type of assessment.
+        </p>
+        <a href="media/ecers.pdf" target="_blank">Check it out!</a>
+    </div>
+
+    <div class="block-item">
+        <h3>AGES AND STAGES QUESTIONARE</h3>
+        <p>
+            This questionare helps identify if a child is meeting certain, developmental milestones.
+            It is important because it helps parents know what specific needs their child may have and offers resources to seek intervention.
+            This questionare demonstraits my ability to communicate with parents and to offer ideas for how they can best help their child.
+        </p>
+        <a href="media/asq.pdf" target="_blank">Check it out!</a>
+    </div>
+
+    <div class="block-item">
+        <h3>observation examples</h3>
+        <p>
+            This is a collection of various observation types including an anecdotal record, running record, frequency sample, and checklist.
+            These are valuable because they help document the behaviors of children.
+            This demonstrates my ability to document my observations in concise, descriptive, and structured ways.
+        </p>
+        <a href="media/observation-assignment.pdf" target="_blank">Check it out!</a>
+    </div>
+
+    <div class="block-item">
+        <h3>assessment comparison chart</h3>
+        <p>
+
+        </p>
+        <a href="media/assessment-comparison-chart-2.pdf" target="_blank">Check it out!</a>
+    </div>
+
+    <div class="block-item">
+        <h3>interview</h3>
+        <p>
+
+        </p>
+        <a href="media/interview.pdf" target="_blank">Check it out!</a>
+    </div>
+
+    <div class="block-item">
+        <h3>other assessments</h3>
+        <p>
+
+        </p>
+        <a href="media/other-assessments.pdf" target="_blank">Check it out!</a>
+    </div>
+`;
+
+const bioImgSrcs = [
+    "media/grad-pic.jpg",
+    "media/nature-large.jpg",
+    "media/temple-marriage.jpg"
+];
+
+const bioTexts = [
+    "I graduated from BYU-Idaho in July of 2025 with a degree in child development.",
+    "I love nature!  I grew up in Kentucky where I spent a lot of time outdoors as a child.  I want to give children the same opportunities to learn from nature that I had.",
+    "I married my husband, Christian, in May of 2023.  He's been my biggest supporter in my persuit of my professional goals."
+];
+
+let bioIndex = 0;
+
+function cycleThroughBio(e) {
+    let iterator = 0;
+    console.log(e.target.innerHTML);
+    if (e.target.innerHTML == '&lt;') {
+        iterator = -1;
+    } else if (e.target.innerHTML == '&gt;') {
+        iterator = 1;
+    }
+    console.log(iterator);
+    bioIndex = (bioIndex + iterator)
+    if (bioIndex >= bioImgSrcs.length) {
+        bioIndex = 0;
+    }
+    if (bioIndex < 0) {
+        bioIndex = bioImgSrcs.length - 1;
+    }
+    console.log(bioIndex);
+    document.getElementById('about-me-img').src = bioImgSrcs[bioIndex];
+    document.getElementById('about-me-text').innerHTML = bioTexts[bioIndex];
+}
+
+const pageContent = document.getElementById('page-content');
+
+function handleNav(e) {
+    const clickedTab = e.target.innerHTML;
+    if (clickedTab == 'About Me') {
+        pageContent.innerHTML = aboutMeContent;
+        document.getElementById('<').addEventListener('click', cycleThroughBio);
+        document.getElementById('>').addEventListener('click', cycleThroughBio);
+    } else if (clickedTab == 'Personal Ethics') {
+        pageContent.innerHTML = personalEthicsContent;
+    } else if (clickedTab == 'Intentional Teacher') {
+        pageContent.innerHTML = intentionalTeacherContent;
+    } else if (clickedTab == 'Skilled Collaborator') {
+        pageContent.innerHTML = skilledCollaboratorContent;
+    } else if (clickedTab == 'Observation and Assessment') {
+        pageContent.innerHTML = observationAndAssessmentContent;
+    }
+}
+
+function toggleScreenSizeWarning() {
+    console.log(window.innerWidth);
+    if (window.innerWidth < 600) {
+        document.getElementById('screen-size-warning').style.display = 'flex';
+        document.getElementById('content').style.display = 'none';
+    } else {
+        document.getElementById('screen-size-warning').style.display = 'none';
+        document.getElementById('content').style.display = 'flex';
+    }
+}
+
+toggleScreenSizeWarning();
+document.addEventListener('click', handleNav);
+window.addEventListener('resize', toggleScreenSizeWarning);
